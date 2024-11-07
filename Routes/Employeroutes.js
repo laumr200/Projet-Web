657b2763c4410ed78f457d4b99bcf5953a0fac47
AIAdriano <165674105+AIAdriano@users.noreply.github.com>
2024-11-07 12:58:41 -0500

    changement2

--- Routes/Employeroutes.js
+++ Routes/Employeroutes.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import express from 'express';
-import { createEmploye, getAllEmployes, getEmployeById, updateEmploye, deleteEmploye } from '../Controllers/employecontroller.js';
-
-const router = express.Router();
-
-// Definir as rotas para employes
-router.post('/', createEmploye);          // Criar novo employe
-router.get('/', getAllEmployes);          // Listar todos os employes
-router.get('/:id', getEmployeById);       // Obter employe específico por ID
-router.put('/:id', updateEmploye);        // Atualizar employe por ID
-router.delete('/:id', deleteEmploye);     // Excluir employe por ID
-
-export default router;
-=======
 import { Router } from 'express';
 import { getAllEmployes, addEmploye, updateEmploye, delEmploye ,getEmployeAbsences, addRoleToEmploye,getEmployeRoles } from '../Controllers/employecontroller.js';
 
@@ -26,4 +11,3 @@ route.post('/:id/roles/:roleId', addRoleToEmploye);
 route.get('/:id/roles', getEmployeRoles);
 
 export default route;
->>>>>>> main
